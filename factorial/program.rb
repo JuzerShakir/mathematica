4dec7d7661e12581a4dfed6952c846661da976c3
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-10-15 10:42:03 +0530

    Removed comments

--- factorial/program.rb
+++ factorial/program.rb
@@ -1,11 +1,8 @@
-# asks user for input
 puts "Enter any positive integer:"
 n = gets.chomp.to_i
 
 if n < 0 then abort("Factorials can only be calculated for positive integers") end
 
-# since our base value 0! = 1, hence we add up starting with 1
-# explained => https://www.youtube.com/watch?v=X32dce7_D48&ab_channel=EddieWoo
 total = 1
 
 1.upto(n) { | i | total *= i }
@@ -14,7 +11,5 @@ puts "\nFactorial of #{n} is #{total}."
 
 ####------------------  or    ------------------------------------------ ####
 
-# gamma is an approximation of a factorial
-# the ans of this method changes slightly compared to above as we give high input numbers
 # while gamma gives result for n-1 by default for gamma(n), we give n+1 to get factorial ofn
 puts "\nFactorial of #{n} is #{Math.gamma(n+1).to_i}"
