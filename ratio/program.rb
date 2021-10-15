8ccd21d833109fdb8bb4be96a70135621c9ee683
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-10-15 23:07:35 +0530

    Avoid dividing by zero, stop program for string input, stop program for when gcd is 1, give simplest_ratio variable

--- ratio/program.rb
+++ ratio/program.rb
@@ -1,15 +1,30 @@
-# asks user for input
 puts "Enter any positive integer:"
 n1, n2 = gets.chomp.split('/').map(&:to_i)
 #p n1, n2
 
-gcd  = n1.gcd(n2)
-#p lcm
+if n2 == 0
+  puts "The value of denominator cannot be 0."
+  exit
+end
 
+begin
+  gcd  = n1.gcd(n2)
+#p gcd
+rescue
+  puts "Unexpected input. Try Again."
+  exit
+end
 
-n1 = n1 / gcd
-n2 = n2 / gcd
+if gcd == 1
+  puts "The greatest common divisor is #{gcd} of #{n1}/#{n2}.
+And it is already in its simplest form."
+  exit
+end
 
-# output as rational number
-puts "The greatest common divisor is #{gcd}
- with the simplest form #{Rational(n1, n2)}"
+simple_n1 = n1 / gcd
+simple_n2 = n2 / gcd
+
+simplest_form = Rational(simple_n1, simple_n2)
+
+puts "The greatest common divisor is #{gcd} of #{n1}/#{n2}.
+And its simplest form is #{simplest_form}."
